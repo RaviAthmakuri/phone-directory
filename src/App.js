f073826cf15681d6605009ce8572bcbf15370759
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-09-09 22:28:46 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Phone Directory
+    <div>
+      {/* <div className="header">
+        Phone Directory
+    </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span><br />
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="Ravi"></input>
     </div>
   );
 }
