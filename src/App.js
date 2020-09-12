b89359244a582615b8b740d2b03de7474d03c278
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-09-13 00:52:27 +0530

    Component Explaination Completed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './Header';
 
-
-function App() {
-  return (
-    <div>
-      {/* <div className="header">
-        Phone Directory
-    </div>
-      <button> Add </button>
+class App extends Component {
+  render() {
+    return (
       <div>
-        <span> Name </span><br />
-        <span> Phone </span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
-      <input id="name" type="text" placeholder="Type Here" defaultValue="Ravi"></input>
-    </div>
-  );
+        <Header />
+        <button> Add </button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
