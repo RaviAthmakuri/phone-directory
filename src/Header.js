39148f3021b11ec3d7b45d8926759058f54429d1
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-09-13 13:14:11 +0530

    Added Styling

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
-const Header = function(){
-    return(
-        <div className="header">
-        Phone Directory
+const Header = function () {
+    const headerStyle = { textAlign: 'center', padding: 20, background: '#000', color: '#fff', textTransform: 'uppercase' };
+    return (
+        <div style={headerStyle}>
+            Phone Directory
         </div>
     )
+
 }
 
-// class Header extends Component {
-//     render() {
-//         return (
-//             <div className="header">
-//                 Phone Directory
-//             </div>
-//         )
-//     }
-// }
+
 
 export default Header;
