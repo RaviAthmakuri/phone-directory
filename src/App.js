20fe88485e4055a3a0a6bc4be6a3661cb5a641e9
ravi.athmakuri <ravi.athmakuri93@gmail.com>
2020-09-13 15:53:59 +0530

    Dynamic Rendering Completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import './App.css';
 
 class App extends Component {
+
   render() {
+    let subscribers = [
+      {
+        id:1,
+        name: "Ravi Atmakuri",
+        phone: "111111111"
+
+      },
+      {
+        id:2,
+        name: "Surabhi Atmakuri",
+        phone: "111345677"
+
+      }
+    ]
     return (
-      <div>
+      <div className="component-body-container">
         <Header />
-        <button> Add </button>
-        <div>
-          <span> Name </span><br />
-          <span> Phone </span>
+        <button className="custom-btn add-btn"> Add </button>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading"> Name </span>
+          <span className="grid-item phone-heading"> Phone </span>
         </div>
-      </div>
+
+        {
+          subscribers.map(sub => {
+            return <div key={sub.id}  className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <span className="grid-item">{sub.phone}</span>
+            </div>
+          })
+        }
+      
+       </div>
+
     );
   }
 }
